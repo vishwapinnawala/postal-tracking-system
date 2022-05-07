28e5bbea9f96fed21912cb3a3bea8f043ff81d3c
vishwapinnawala <vishwaspinnawala@gmail.com>
2022-05-07 18:53:35 +0530

    Initial Commit

---
+++ app/src/main/java/com/postaltracking/lk/postofficenav.kt
@@ -0,0 +1,22 @@
+package com.postaltracking.lk
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.postaltracking.lk.databinding.ActivityPostofficedashBinding
+import com.postaltracking.lk.databinding.ActivityPostofficenavBinding
+
+class postofficenav : AppCompatActivity() {
+    private lateinit var binding : ActivityPostofficenavBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        binding= ActivityPostofficenavBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button3.setOnClickListener{
+            val intent = Intent(this, postofficedash::class.java)
+            startActivity(intent)
+        }
+    }
+}
